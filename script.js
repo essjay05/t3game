90c4a0b77c47c503ebf875aee1f2e0ad644fd102
essjay05 <stepjoyous@gmail.com>
2019-07-22 20:19:50 -0700

    Added index.html, script.js, style.css

---
+++ script.js
@@ -0,0 +1,16 @@
+var $box = $('.box');
+var player = 'X';
+var clickedBoxes = [];
+var xMoves = [];
+var oMoves = [];
+console.log($box);
+
+$box.click(function() {
+    var boxId = (event.target.id);
+    alert('Clicked ' + boxId);
+    // Check if boxId is in clickedBoxes array
+     
+        // if 
+
+        // possibly use filter to create winning array
+});
