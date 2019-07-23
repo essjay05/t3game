c9c825f998260eed257bb0531eb9ab5aeb41477a
essjay05 <stepjoyous@gmail.com>
2019-07-22 21:44:12 -0700

    Tweaked CSS to make game fit on one screen, javascript works for click to insert X or O into innerHTML and switches with each turn.

--- script.js
+++ script.js
@@ -7,10 +7,20 @@ console.log($box);
 
 $box.click(function() {
     var boxId = (event.target.id);
-    alert('Clicked ' + boxId);
+    console.log(`Clicked boxID is ${boxId} and player is ${player} and it is ${clickedBoxes.includes(boxId)} that this ID is in the clickedBoxes array`);
+    
     // Check if boxId is in clickedBoxes array
-     
-        // if 
-
-        // possibly use filter to create winning array
+    if (clickedBoxes.includes(boxId) != 'true' && player === 'X') {
+        document.getElementById(boxId).innerHTML = player;
+        clickedBoxes.push(boxId);
+        player = 'O';
+        console.log(`inside IF Clicked boxID is ${boxId} and player is ${player}`);
+    } else if (clickedBoxes.includes(boxId) != 'true' && player === 'O') {
+        document.getElementById(boxId).innerHTML = player;
+        player = 'X';  
+        console.log(`inside elseIF Clicked boxID is ${boxId} and player is ${player}`)
+    } else if (clickedBoxes.includes(boxId) == 'false' ) {
+        console.log('Box is taken, please choose a different box.')
+    };
+    console.log(`Clicked boxes: ${clickedBoxes}`);
 });
