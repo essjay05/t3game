a760b77d34b4563e1d7a2f95e973b9fb6dec399e
essjay05 <stepjoyous@gmail.com>
2019-07-22 22:00:28 -0700

    Successfully got functionality for not being able to select the same box that a player has previously chosen. Adds selected box to player's moves array.

--- script.js
+++ script.js
@@ -3,24 +3,27 @@ var player = 'X';
 var clickedBoxes = [];
 var xMoves = [];
 var oMoves = [];
+
 console.log($box);
 
 $box.click(function() {
     var boxId = (event.target.id);
-    console.log(`Clicked boxID is ${boxId} and player is ${player} and it is ${clickedBoxes.includes(boxId)} that this ID is in the clickedBoxes array`);
+    var b = clickedBoxes.includes(boxId);
+    // console.log(`Clicked boxID is ${boxId} and player is ${player} and it is ${clickedBoxes.includes(boxId)} that this ID is in the clickedBoxes array`);
     
     // Check if boxId is in clickedBoxes array
-    if (clickedBoxes.includes(boxId) != 'true' && player === 'X') {
+    if (b != true && player === 'X') {
         document.getElementById(boxId).innerHTML = player;
         clickedBoxes.push(boxId);
+        xMoves.push(boxId);
         player = 'O';
-        console.log(`inside IF Clicked boxID is ${boxId} and player is ${player}`);
-    } else if (clickedBoxes.includes(boxId) != 'true' && player === 'O') {
+    } else if (b != true && player === 'O') {
         document.getElementById(boxId).innerHTML = player;
+        clickedBoxes.push(boxId);
+        oMoves.push(boxId);
         player = 'X';  
-        console.log(`inside elseIF Clicked boxID is ${boxId} and player is ${player}`)
-    } else if (clickedBoxes.includes(boxId) == 'false' ) {
-        console.log('Box is taken, please choose a different box.')
+    } else {
+        alert('Box is taken, please choose a different box.')
     };
-    console.log(`Clicked boxes: ${clickedBoxes}`);
+    console.log(`Clicked boxes: ${clickedBoxes}, X has boxes: ${xMoves}, and O has boxes: ${oMoves}.`);
 });
