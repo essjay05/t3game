8056cb16278b8ca2d9c19224be28cae2f978817d
essjay05 <stepjoyous@gmail.com>
2019-07-22 22:04:27 -0700

    Added pseudocode notes to javascript for choosing box functionality.

--- script.js
+++ script.js
@@ -11,16 +11,25 @@ $box.click(function() {
     var b = clickedBoxes.includes(boxId);
     // console.log(`Clicked boxID is ${boxId} and player is ${player} and it is ${clickedBoxes.includes(boxId)} that this ID is in the clickedBoxes array`);
     
-    // Check if boxId is in clickedBoxes array
+    // Check if boxId is in clickedBoxes array and if player is X
     if (b != true && player === 'X') {
+        // Insert X into selected box
         document.getElementById(boxId).innerHTML = player;
+        // Add box ID to clickedBoxes array
         clickedBoxes.push(boxId);
+        // Add box Id to player xMoves array
         xMoves.push(boxId);
+        // Switch to player O
         player = 'O';
+    // Check if boxId is in clickedBoxes array and if player is O
     } else if (b != true && player === 'O') {
+        // Insert X into selected box
         document.getElementById(boxId).innerHTML = player;
+        // Add box ID to clickedBoxes array
         clickedBoxes.push(boxId);
+        // Add box Id to player xMoves array
         oMoves.push(boxId);
+        // Switch to player X
         player = 'X';  
     } else {
         alert('Box is taken, please choose a different box.')
