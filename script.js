0689a78f88e3b13a04397f5e410bdc3528a7d742
essjay05 <stepjoyous@gmail.com>
2019-07-31 11:54:25 -0700

    Added check win functionality successfully... need to update score board innerHTML with new scores once won.

--- script.js
+++ script.js
@@ -1,21 +1,66 @@
-var $box = $('.box');
-var player = 'X';
-var clickedBoxes = [];
-var xMoves = [];
-var oMoves = [];
-// var winningCombos = [['A1','B1','C1'], 
-//                     ['A2','B2','C2'],
-//                     ['A3','B3','C3'],
-//                     ['A1','A2','A3'],
-//                     ['B1','B2','B3'],
-//                     ['C1','C2','C3'],
-//                     ['A1','B2','C3'],
-//                     ['C1','B2','A3']]
+var $box = $('.box'),
+    box = document.getElementsByClassName('box'),
+    player = 'X',
+    clickedBoxes = [],
+    xMoves = [],
+    oMoves = [],
+    xScore = 0,
+    $xScore = document.getElementById('scoreX');
+    oScore = 0,
+    winningCombos = [['A1','B1','C1'],
+                    ['A2','B2','C2'],
+                    ['A3','B3','C3'],
+                    ['A1','A2','A3'],
+                    ['B1','B2','B3'],
+                    ['C1','C2','C3'],
+                    ['A1','B2','C3'],
+                    ['C1','B2','A3']];
 
-console.log($box);
+console.log(box);
 
-function checkWin() {
-
+function checkWin(bId, player, pScore) {
+    console.log('inside checkWin');
+    console.log('Box ID: ' + bId);
+    console.log('Player: ' + player);
+    console.log($box[0].innerHTML);
+    // Check for Row wins:
+    if ( $box[0].innerHTML == player && $box[1].innerHTML == player && $box[2].innerHTML == player ) {
+        console.log(player + ' Wins!');
+        return pScore++;
+        console.log(`${player} score is now ${pScore}`);
+    } else if ( $box[3].innerHTML == player && $box[4].innerHTML == player && $box[5].innerHTML == player ) {
+        console.log(player + ' Wins!');
+        return pScore++;
+        console.log(`${player} score is now ${pScore}`);
+    } else if ( $box[6].innerHTML == player && $box[7].innerHTML == player && $box[8].innerHTML == player ) {
+        console.log(player + ' Wins!');
+        pScore++;
+        console.log(`${player} score is now ${pScore}`);
+    // Check for Column wins:
+    } else if ( $box[0].innerHTML == player && $box[3].innerHTML == player && $box[6].innerHTML == player ) {
+        console.log(player + ' Wins!');
+        pScore++;
+        console.log(`${player} score is now ${pScore}`);
+    } else if ( $box[1].innerHTML == player && $box[4].innerHTML == player && $box[7].innerHTML == player ) {
+        console.log(player + ' Wins!');
+        pScore++;
+        console.log(`${player} score is now ${pScore}`);
+    } else if ( $box[2].innerHTML == player && $box[5].innerHTML == player && $box[8].innerHTML == player ) {
+        console.log(player + ' Wins!');
+        pScore++;
+        console.log(`${player} score is now ${pScore}`);
+    // Check for Diagonal wins
+    } else if ( $box[0].innerHTML == player && $box[4].innerHTML == player && $box[8].innerHTML == player ) {
+        console.log(player + ' Wins!');
+        pScore++;
+        console.log(`${player} score is now ${pScore}`);
+    } else if ( $box[2].innerHTML == player && $box[4].innerHTML == player && $box[6].innerHTML == player ) {
+        console.log(player + ' Wins!');
+        pScore++;
+        console.log(`${player} score is now ${pScore}`);
+    } else {
+        console.log(`It's a tie!`);
+    }
 }
 
 function resetGame() {
@@ -41,9 +86,12 @@ $box.click(function() {
         // Add box Id to player xMoves array
         xMoves.push(boxId);
             // Check if xMoves has a winning combo
-            
+            checkWin(boxId, player, xScore);
+            console.log('xScore is: ' + xScore);
+            $xScore.innerHTML = xScore;
         // Switch to player O
         player = 'O';
+        
     // Check if boxId is in clickedBoxes array and if player is O
     } else if (b != true && player === 'O') {
         // Insert X into selected box
@@ -53,6 +101,8 @@ $box.click(function() {
         // Add box Id to player xMoves array
         oMoves.push(boxId);
             // Check if oMoves has a winning combo
+            checkWin(boxId, player, oScore);
+            $xScore.innerHTML = oScore;
         // Switch to player X
         player = 'X';  
     } else {
@@ -60,3 +110,53 @@ $box.click(function() {
     };
     console.log(`Clicked boxes: ${clickedBoxes}, X has boxes: ${xMoves}, and O has boxes: ${oMoves}.`);
 });
+
+
+
+
+
+
+
+
+// Failed Check Win JS tests/code:
+
+    // wCombos;
+    // Loop into wCombos:
+    // for (let w = 0; w <= wCombos.length; w++) {
+        //     let ticTacToe = 0;
+        //     // Check if pMoves box is in combo
+        //     if (wCombos[w].includes(pMoves) == true) {
+        //         console.log('pMoves= ' + pMoves )
+        //         ticTacToe++;
+        //         console.log('ticTacToe= ' + ticTacToe );
+        //         console.log('Player moves: ' + pMoves + ' | ' + 'WComBos: ' + wCombos )
+        //     } return
+        // }
+        //     console.log(wCombos[w]);
+    
+            // Loop into winning combo 3 set array
+        //     for (let b = 1; b <= wCombos[w].length; b++) {
+        //         // Check if each element of char moves is in
+        //             // winning array,
+        //         let ticTacToe = 0;
+        //         if (wCombos[w].includes(pMoves) == true) {
+        //             ticTacToe++;
+        //             if (ticTacToe == 3) {
+        //                 console.log(`This player won!`)
+        //             }
+        //         } else {
+        //             console.log(`This winning combo matched ${ticTacToe} of player's moves`)
+        //         };
+    
+                
+        //     }
+        // }
+    
+    
+        // if (0 === wCombos.length) {
+        //     console.log('No Win' + 'Superset: ' + pMoves + ' Subset: ' + wCombos);
+        //     console.log(false);
+        // } return wCombos.every(function (box) {
+        //     console.log('Superset: ' + pMoves + ' Subset: ' + wCombos);
+        //     console.log(pMoves.indexOf(box) >= 0 + 'array2 in array1');
+        // });
